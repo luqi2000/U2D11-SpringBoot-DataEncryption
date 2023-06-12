e6fe3d12a0d16388fddc8b826eb4d4d2e6c5fec5
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-12 12:58:05 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D11SpringBootDataEncryption/U2D11SpringBootDataEncryptionApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D11SpringBootDataEncryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D11SpringBootDataEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D11SpringBootDataEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D11SpringBootDataEncryption/U2D11SpringBootDataEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D11SpringBootDataEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D11SpringBootDataEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
